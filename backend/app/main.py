42e651f49a1c4235c22226a93a943f6e2c375ce7
Azim Hatami <azimhatami4@gmail.com>
2024-11-29 17:42:55 +0330

    Fix: Cross-Origin Resource Sharing error

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.todo import todo_router
 
@@ -7,6 +8,19 @@ from app.routers.todo import todo_router
 # App object
 app = FastAPI()
 app.include_router(todo_router)
+
+origins = [
+    'http://localhost',
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def root():
