1c85951328a0e6b61332712ff5eef17abbd98d00
Azim Hatami <azimhatami4@gmail.com>
2024-12-03 23:34:45 +0330

    Added ability to mark todos as completed

--- frontend/src/components/TodoListView.jsx
+++ frontend/src/components/TodoListView.jsx
@@ -1,4 +1,5 @@
 import { MdRadioButtonUnchecked } from "react-icons/md";
+import { IoMdCheckmarkCircle } from "react-icons/io";
 import { useState, useEffect } from 'react'
 import TodoAdd from './TodoAdd'
 import TodoDelete from './TodoDelete'
@@ -7,28 +8,55 @@ import axios from 'axios'
 
 function TodoListView({ isLoading, error, todos, setTodos }) {
 
+	const [completed, setCompleted] = useState(false);
+
+	const handleCompleted = (e, todoId) => {
+		e.preventDefault();
+		setTodos(prevTodos => prevTodos.map((todo) => todo._id === todoId ? { ...todo, completed: !todo.completed } : todo))
+	};
+
 	return(
 		<>
 			<h2 className='font-bold text-[5rem] font-mono mb-[1rem]'>Todo List</h2>
-			<div className='w-[60%] m-auto'>
+			<div className='w-full grid grid-cols-3'>
 				<TodoAdd todos={ todos } setTodos={ setTodos } />
-				<div>
+				<div className='col-span-2 w-[42rem] justify-self-end'>
 					{ 
 					  isLoading ? (
 					    <p>Loading...</p>
 					) : error ? (
 					    <p>Error: {error}</p>
 					) : (
-					  	  <ul className='flex flex-col gap-2 mt-[1rem]'>
+					  	  <ul className='flex flex-col gap-3'>
 					  		{todos.map((todo) => {
 								return(
 									<li 
 										key={todo._id} 
-										className='bg-zinc-900 hover:bg-zinc-800 h-[3rem] rounded-lg flex flex-row items-center justify-between px-4 font-semibold'
+										className={
+											`${
+												todo.completed ? (
+													'bg-zinc-800 line-through') : (
+														'')
+											} bg-zinc-900 h-[3rem] rounded-lg flex flex-row items-center justify-between px-4 h-[4.5rem]`}
 									>
-										<MdRadioButtonUnchecked className='text-[1.3rem]'/>
-										<div>{todo.title}</div>
-										<TodoDelete todo_id={ todo._id } todos={ todos } setTodos={ setTodos } />
+										<div className='flex flex-col text-start gap-1'>
+											<div className='text-lg font-bold'>{todo.title}</div>
+											<div className='text-sm text-slate-400 font-semibold'>{todo.description}</div>
+										</div>
+										<div className='flex flex-row items-center gap-4'>
+											<button onClick={(e) => handleCompleted(e, todo._id)} className='text-sky-400 outline-none'>
+												{
+													todo.completed ?
+													<IoMdCheckmarkCircle
+														className='text-[1.5rem]'
+													/> :
+													<MdRadioButtonUnchecked 
+														className='text-[1.5rem]'
+													/>
+												}
+											</button>
+											<TodoDelete todo_id={ todo._id } todos={ todos } setTodos={ setTodos } />
+										</div>
 									</li>
 								)
 							})}
